ef14ddb4721baa441f245cdad6b09270b94f6532
Atxada <atxadaaldo17022@gmail.com>
2025-01-31 21:46:38 +0700

    update project page

--- app/(blog)/articles/[slug]/page.jsx
+++ app/(blog)/articles/[slug]/page.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import Blog from '@components/Blog';
+import { getAllPosts } from '@utils/posts';
+
+export const generateStaticParams = () => { // generate params at build time rather than on click
+    const posts = getAllPosts();
+    const articlePosts = posts.filter((post) => post.category === "articles")
+    const paths = Object.keys(articlePosts).map(path => ({path: slug(path)}));
+    return paths;
+}
+
 
 const page = ({params}) => {
   return (
--- app/(blog)/projects/[slug]/page.jsx
+++ app/(blog)/projects/[slug]/page.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import Blog from '@components/Blog';
 
+export const generateStaticParams = () => { // generate params at build time rather than on click
+    const posts = getAllPosts();
+    const articlePosts = posts.filter((post) => post.category === "projects")
+    const paths = Object.keys(articlePosts).map(path => ({path: slug(path)}));
+    return paths;
+}
+
 const page = ({params}) => {
   return (
     <Blog params={params}/>
